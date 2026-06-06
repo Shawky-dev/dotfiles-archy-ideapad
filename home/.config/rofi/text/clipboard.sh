e43da238744ca3b1b65fed506f8a92960ba80dda
shawky-dev <shawky.projects25@gmail.com>
2026-06-06 18:38:46 +0300

    clipboard history replace greenclip with copyq

--- home/.config/rofi/text/clipboard.sh
+++ home/.config/rofi/text/clipboard.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python3
 
 ## Author : Aditya Shakya (adi1090x)
 ## Github : @adi1090x
@@ -11,10 +11,31 @@
 ## style-6     style-7     style-8     style-9     style-10
 ## style-11    style-12    style-13    style-14    style-15
 
-dir="$HOME/.config/rofi/text/"
-theme='style-5'
+import json
+import subprocess as sp
 
-## Run
-rofi \
-    -dmenu -i -p " "\
-    -theme ${dir}/${theme}.rasi
+copyq_script = r"""
+var result=[];
+for (var i = 0; i < size(); ++i) {
+  var obj = {};
+  obj.row = i;
+  obj.text = str(read(i));
+  result.push(obj);
+}
+JSON.stringify(result);
+"""
+
+p = sp.run(['copyq', '-'], input=copyq_script, encoding='utf-8', stdout=sp.PIPE, stderr=sp.PIPE)
+items = json.loads(p.stdout)
+
+# Collapse newlines for display only
+display = [" ".join(obj['text'].replace("\n", " ").split()) for obj in items]
+
+rofi = ['rofi', '-dmenu', '-i', '-p', ' ', '-format', 'i',
+        '-theme', '/home/shawky/.config/rofi/text/style-5.rasi']
+
+p = sp.run(rofi, input='\n'.join(display), encoding='utf-8', stdout=sp.PIPE)
+
+if p.returncode == 0:
+    idx = p.stdout.strip()
+    sp.run(f'copyq select({idx});', shell=True)
--- home/scripts/clear-clipboard.sh
+++ home/scripts/clear-clipboard.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
-
-# Kill greenclip daemon
-pkill greenclip
-
-# Clear clipboard
+# Clear copyq history
+copyq remove $(seq 0 $(copyq count))
+# Clear system clipboard
 xclip -selection clipboard -i /dev/null
 xclip -selection primary -i /dev/null
-
-# Clear greenclip history file
-rm -f ~/.cache/greenclip.history
-
-# Restart greenclip
-sleep 0.5
-greenclip daemon > /dev/null 2>&1 &
-
 notify-send "Clipboard history cleared"
